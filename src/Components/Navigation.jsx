504c33e47d99f8060065abb90c80fd1d711dcb99
Rahmat Fajar Saputra <fajar.saputra2907@gmail.com>
2025-06-14 12:31:10 +0700

    done

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,15 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
+    const navigate = useNavigate();
+
     return (
         <>
             <nav role="navigation" aria-label="Main navigation">
                 <div class="brand" tabindex="0">RenyHijab</div>
                 <div class="nav-links" id="navLinks">
-                    <a href="/" tabindex="0">Home</a>
-                    <a href="/product" tabindex="0">Product</a>
-                    <a href="/transaction" tabindex="0">Transaction</a>
-                    <a href="#" tabindex="0">Customer</a>
+                    <a onClick={() => navigate('/')} tabindex="0">Home</a>
+                    <a onClick={() => navigate('/product')} tabindex="0">Product</a>
+                    <a onClick={() => navigate('/transaction')} tabindex="0">Transaction</a>
+                    <a onClick={() => navigate('/')} tabindex="0">Customer</a>
                 </div>
             </nav><br /><br />
         </>
