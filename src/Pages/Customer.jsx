fae292708071c5f7c675bd16c3119880fbd247e0
Rahmat Fajar Saputra <fajar.saputra2907@gmail.com>
2025-06-14 18:28:03 +0700

    done commit

--- src/Pages/Customer.jsx
+++ src/Pages/Customer.jsx
@@ -31,6 +31,27 @@ function Customer() {
                   <button class="btn btn-secondary">Hapus</button>
                 </td>
               </tr>
+              <tr>
+                <td>Nisa</td>
+                <td>
+                  <button class="btn btn-primary">Edit</button>
+                  <button class="btn btn-secondary">Hapus</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Hapis</td>
+                <td>
+                  <button class="btn btn-primary">Edit</button>
+                  <button class="btn btn-secondary">Hapus</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Ucup</td>
+                <td>
+                  <button class="btn btn-primary">Edit</button>
+                  <button class="btn btn-secondary">Hapus</button>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
